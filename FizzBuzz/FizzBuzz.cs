641c0a6dc6557011f159b9a4f9fd312b3ee868e7
Dorota Kmiecik <dorota.kmiecik87@gmail.com>
2023-05-24 22:18:48 +0200

    Adding files

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class FizzBuzz
+    {
+        public FizzBuzz()
+
+        {
+            try
+            {
+
+
+                while (true)
+                {
+                    Console.WriteLine("Podaj jakas liczbe calkowita");
+                    var number = int.Parse(Console.ReadLine());
+                    if (number % 3 == 0 & number % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                        break;
+                    }
+                    else if (number % 3 == 0 & number % 5 != 0)
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                    else if (number % 5 == 0 & number % 3 != 0)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Twoja liczba to:" + number.ToString());
+                    }
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally 
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fizzbuzz = new FizzBuzz();
+        }
+    }
+}
